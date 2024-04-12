2e00abc49eaa2fdabbf97bd4f7b8ce0236215d4b
Manuel2595 <0001038520@senaimgaluno.com.br>
2024-04-12 07:59:17 -0300

    Finalização da pagina de contato

--- my-ecommerce/src/Components/contact/Contact.jsx
+++ my-ecommerce/src/Components/contact/Contact.jsx
@@ -1,11 +1,13 @@
 import './Contact.css';
 import ContactHeader from './ContactHeader';
 import ContactForm from './ContactForm';
+import ContactMaps from './ContactMaps';
 const contact = () => {
   return (
     <div className='contact'>
       <ContactHeader/>
       <ContactForm/>
+      <ContactMaps/>
     </div>
   )
 }
--- my-ecommerce/src/Components/contact/ContactMaps.jsx
+++ my-ecommerce/src/Components/contact/ContactMaps.jsx
@@ -1,11 +1,12 @@
-
-
+import './Contact.css';
 const ContactMaps = () => {
   return (
-    <div>
-      
+    <div className='MapsContainner'>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722.004971379486!2d-42.980800126389674!3d-21.112367976211015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa31b824cdaf345%3A0x5d61523a7b8528d2!2sAv.%20Pres.%20Juscelino%20Kubitscheck%20de%20Oliveira%2C%202326%20-%20Ub%C3%A1%2C%20MG%2C%2036507-330!5e0!3m2!1spt-BR!2sbr!4v1712917954175!5m2!1spt-BR!2sbr"
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
-export default ContactMaps
+export default ContactMaps;
